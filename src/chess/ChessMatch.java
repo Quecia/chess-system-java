a4e501d312614dbad65dfeb9a610bc26fe635929
Quecia <marquesquecia23@gmail.com>
2020-04-09 10:56:21 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -85,7 +85,8 @@ public class ChessMatch {/*Partida de xadrez*/
 	}
 	
 	private Piece makeMove(Position source, Position target) {/*Realizar movimento*/
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
@@ -98,7 +99,8 @@ public class ChessMatch {/*Partida de xadrez*/
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {/*Desfazer movimento*/
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		
 		if(capturedPiece != null) {
